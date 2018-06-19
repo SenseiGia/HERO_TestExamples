8aa12a3dfb84457cf70ce76bea87229d7b880dad
SenseiGia <Gia.resendiz@gmail.com>
2018-06-19 16:58:43 -0400

    Updated Project to test latest update to fix

--- TalonsEverywhere/Program.cs
+++ TalonsEverywhere/Program.cs
@@ -11,8 +11,8 @@ namespace TalonsEverywhere
     public class Program
     {
         /* Values used to talons */
-        static int kTimeout = 14;
-        static int kNumOfTalons = 17;
+        static int kTimeout = 10;
+        static int kNumOfTalons = 15;
         static TalonSRX[] _talonCollection = new TalonSRX[kNumOfTalons];
         static int _talonStartIndex = 1;
 
@@ -23,91 +23,74 @@ namespace TalonsEverywhere
 
         public static void Main()
         {
-            /* Create our 12 Talons... */
+            /* Create Talons */
             for (int i = 0; i < (kNumOfTalons); i++)
             {
                 _talonCollection[i] = new TalonSRX(i + _talonStartIndex);
             }
-            foreach (TalonSRX _talon in _talonCollection)
-                _talon.SetStatusFramePeriod(StatusFrameEnhanced.Status_7_CommStatus, 250, kTimeout);
-            /* Everything should be default */
+
+            ///* Set Status Frame 7, Check for Dropped Transmit */
+            //foreach (TalonSRX _talon in _talonCollection)
+            //    _talon.SetStatusFramePeriod(StatusFrameEnhanced.Status_7_CommStatus, 50, kTimeout);
+
+            /* Init done, indicate current timeout value */
             Debug.Print("Teleop Init completed, timeout: " + kTimeout);
 
             while (true)
             {
+                /* Reset errorDetected and error */
                 CTRE.ErrorCode error;
                 bool errorDetected = false;
+
+                CTRE.Phoenix.Watchdog.Feed();
 
                 /* Second indexing method */
                 if (talonCount < kNumOfTalons)
                 {
+                    /* Get current Talon Device ID */
                     int talonNumber = _talonCollection[talonCount].GetDeviceID();
 
-                    /* Do 10 configs per talon */
                     error = _talonCollection[talonCount].Config_kP(0, 1, kTimeout);                         // 310
                     if (error != CTRE.ErrorCode.OK)
-                        //if (error != CTRE.ErrorCode.CAN_TX_FULL)
                         errorDetected = true;
                     error = _talonCollection[talonCount].Config_kI(0, 1, kTimeout);                         // 311
                     if (error != CTRE.ErrorCode.OK)
-                        //if (error != CTRE.ErrorCode.CAN_TX_FULL)
                         errorDetected = true;
                     error = _talonCollection[talonCount].Config_kD(0, 1, kTimeout);                         // 312
                     if (error != CTRE.ErrorCode.OK)
-                        //if (error != CTRE.ErrorCode.CAN_TX_FULL)
                         errorDetected = true;
                     error = _talonCollection[talonCount].Config_kF(0, 1, kTimeout);                         // 313
                     if (error != CTRE.ErrorCode.OK)
-                        //if (error != CTRE.ErrorCode.CAN_TX_FULL)
                         errorDetected = true;
                     error = _talonCollection[talonCount].Config_IntegralZone(0, 100, kTimeout);             // 314
                     if (error != CTRE.ErrorCode.OK)
-                        //if (error != CTRE.ErrorCode.CAN_TX_FULL)
                         errorDetected = true;
                     error = _talonCollection[talonCount].ConfigAllowableClosedloopError(0, 100, kTimeout);  // 315
                     if (error != CTRE.ErrorCode.OK)
-                        //if (error != CTRE.ErrorCode.CAN_TX_FULL)
                         errorDetected = true;
-                    //error = _talonCollection[talonCount].ConfigContinuousCurrentLimit(10, kTimeout);        // 360
-                    //if (error != CTRE.ErrorCode.OK)
-                    //    //if (error != CTRE.ErrorCode.CAN_TX_FULL)
-                    //    errorDetected = true;
-                    //error = _talonCollection[talonCount].ConfigPeakCurrentDuration(10, kTimeout);           // 361
-                    //if (error != CTRE.ErrorCode.OK)
-                    //    //if (error != CTRE.ErrorCode.CAN_TX_FULL)
-                    //    errorDetected = true;
-                    //error = _talonCollection[talonCount].ConfigPeakCurrentLimit(15, kTimeout);              // 362
-                    //if (error != CTRE.ErrorCode.OK)
-                    //    //if (error != CTRE.ErrorCode.CAN_TX_FULL)
-                    //    errorDetected = true;
                     error = _talonCollection[talonCount].ConfigNominalOutputForward(0.5f, kTimeout);        // 306
                     if (error != CTRE.ErrorCode.OK)
-                        //if (error != CTRE.ErrorCode.CAN_TX_FULL)
                         errorDetected = true;
                     error = _talonCollection[talonCount].ConfigNominalOutputReverse(-0.5f, kTimeout);       // 308
                     if (error != CTRE.ErrorCode.OK)
-                        //if (error != CTRE.ErrorCode.CAN_TX_FULL)
                         errorDetected = true;
                     error = _talonCollection[talonCount].ConfigPeakOutputForward(1, kTimeout);              // 305
                     if (error != CTRE.ErrorCode.OK)
-                        //if (error != CTRE.ErrorCode.CAN_TX_FULL)
                         errorDetected = true;
                     error = _talonCollection[talonCount].ConfigPeakOutputReverse(-1, kTimeout);             // 307
                     if (error != CTRE.ErrorCode.OK)
-                        //if (error != CTRE.ErrorCode.CAN_TX_FULL)
                         errorDetected = true;
-
-                    ///* just see how fast this loop goes */
-                    //byte[] temp1 = new byte[8];
-                    //ulong data1 = (ulong)BitConverter.ToUInt64(temp1, 0);
-                    //CTRE.Native.CAN.Send(0x0000A00A, data1, 8, 0);
 
                     if (!errorDetected)
                         passCount++;
                     else if (errorDetected)
                         failCount++;
-                    
-                    Debug.Print("Config Success Count: " + passCount + " Config Fail Count: " + failCount + " Talon #: " + talonNumber);
+
+                    byte[] temp1 = new byte[8];
+                    ulong data1 = (ulong)BitConverter.ToUInt64(temp1, 0);
+                    CTRE.Native.CAN.Send(0x0000A00A, data1, 8, 0);
+
+                    Debug.Print("Success Count: " + passCount + " Fail Count: " + failCount + " Talon #: " + talonNumber);
                     talonCount++;
                 }
                 else
@@ -115,7 +98,7 @@ namespace TalonsEverywhere
                     talonCount = 0;
                 }
 
-                Thread.Sleep(5);
+                Thread.Sleep(10);
             }
         }
     }
